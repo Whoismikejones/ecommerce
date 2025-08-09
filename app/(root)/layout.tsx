f3fce57d54fa081aa9ff609ffdab67146a0c4d5c
Whoismikejones <whoismikejones25@gmail.com>
2025-08-09 15:38:41 -0500

    header and footer progress

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
-import Header from "@/components/shared/header"
+import Header from '@/components/shared/header';
+import '@/app/globals.css';
+import Footer from '@/components/footer';
 
 export default function RootLayout({
   children,
@@ -6,12 +8,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   <div className='flex h-screen flex-col'>
-    <Header />
- 
-    <main className='flex-1 wrapper'>
-        {children}
-    </main>
-   </div>
+    <div className='flex h-screen flex-col'>
+      <Header />
+      <main className='flex-1 wrapper'>{children}</main>
+      <Footer />
+    </div>
   );
 }
