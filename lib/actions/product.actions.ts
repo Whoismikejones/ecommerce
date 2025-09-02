f0d2979e65d8296e8228f91456bf00e9b7452c0f
Whoismikejones <whoismikejones25@gmail.com>
2025-09-01 19:59:30 -0500

    section3 25

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -10,7 +10,7 @@ import { PrismaClient } from '@prisma/client';
 
 // Get latest products
 export async function getLatestProducts() {
-  //const prisma = new PrismaClient();
+ 
 
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT, 
@@ -21,10 +21,10 @@ export async function getLatestProducts() {
     return convertToPlainObject(data);
 }
 
-
 // Get single product by it's slug
 export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug: slug },
   });
 }
+
