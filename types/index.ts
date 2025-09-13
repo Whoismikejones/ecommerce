6a58399010de372261bc0ad296a763a1496e1744
Whoismikejones <whoismikejones25@gmail.com>
2025-09-13 14:23:09 -0500

    section5 2

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,14 @@
 import { z } from 'zod';
-import { insertProductSchema } from '@/lib/validators';
+import {
+  insertProductSchema,
+  insertCartSchema,
+  cartItemSchema,
+//  shippingAddressSchema,
+//  insertOrderItemSchema,
+//  insertOrderSchema,
+//  paymentResultSchema,
+//  insertReviewSchema,
+} from '@/lib/validators';
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
@@ -7,3 +16,6 @@ export type Product = z.infer<typeof insertProductSchema> & {
   numReviews: number;
   createdAt: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
