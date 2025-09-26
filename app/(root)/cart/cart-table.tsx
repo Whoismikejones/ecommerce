391326271bd5211969117a603896f2aa01ce5276
Whoismikejones <whoismikejones25@gmail.com>
2025-09-25 19:00:18 -0500

    section6 58

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -14,15 +14,17 @@ import {
   TableHeader,
   TableRow,
   TableCell,
-} from '@/components/ui/table';
+} from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-// import { formatCurrency } from '@/lib/utils';
+import { formatCurrency } from '@/lib/utils';
+
+/*Check Out*/
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const router = useRouter();
-const { toast } = useToast();
-const [isPending, startTransition] = useTransition();
+  const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
   return (
     <>
       <h1 className="py-4 h2-bold">Shopping Cart</h1>
@@ -32,14 +34,14 @@ const [isPending, startTransition] = useTransition();
         </div>
       ) : (
         //set the table for cart
-       <div className='grid md:grid-cols-4 md:gap-5'>
-          <div className='overflow-x-auto md:col-span-3'>
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
             <Table>
               <TableHeader>
                 <TableRow>
                   <TableHead>Item</TableHead>
-                  <TableHead className='text-center'>Quantity</TableHead>
-                  <TableHead className='text-right'>Price</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
@@ -48,7 +50,7 @@ const [isPending, startTransition] = useTransition();
                     <TableCell>
                       <Link
                         href={`/product/${item.slug}`}
-                        className='flex items-center'
+                        className="flex items-center"
                       >
                         <Image
                           src={item.image}
@@ -56,57 +58,59 @@ const [isPending, startTransition] = useTransition();
                           width={50}
                           height={50}
                         />
-                        <span className='px-2'>{item.name}</span>
+                        <span className="px-2">{item.name}</span>
                       </Link>
                     </TableCell>
-                    <TableCell className='flex-center gap-2'>
-  <Button
-    disabled={isPending}
-    variant='outline'
-    type='button'
-    onClick={() =>
-      startTransition(async () => {
-        const res = await removeItemFromCart(item.productId);
-        if (!res.success) {
-          toast({
-            variant: 'destructive',
-            description: res.message,
-          });
-        }
-      })
-    }
-  >
-    {isPending ? (
-      <Loader className='w-4 h-4  animate-spin' />
-    ) : (
-      <Minus className='w-4 h-4' />
-    )}
-  </Button>
-  <span>{item.qty}</span>
-  <Button
-    disabled={isPending}
-    variant='outline'
-    type='button'
-    onClick={() =>
-      startTransition(async () => {
-        const res = await addItemToCart(item);
-        if (!res.success) {
-          toast({
-            variant: 'destructive',
-            description: res.message,
-          });
-        }
-      })
-    }
-  >
-    {isPending ? (
-      <Loader className='w-4 h-4  animate-spin' />
-    ) : (
-      <Plus className='w-4 h-4' />
-    )}
-  </Button>
-</TableCell>
-                    <TableCell className='text-right'>${item.price}</TableCell>
+                    <TableCell className="flex-center gap-2">
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await removeItemFromCart(
+                              item.productId
+                            );
+                            if (!res.success) {
+                              toast({
+                                variant: "destructive",
+                                description: res.message,
+                              });
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4  animate-spin" />
+                        ) : (
+                          <Minus className="w-4 h-4" />
+                        )}
+                      </Button>
+                      <span>{item.qty}</span>
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await addItemToCart(item);
+                            if (!res.success) {
+                              toast({
+                                variant: "destructive",
+                                description: res.message,
+                              });
+                            }
+                          })
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4  animate-spin" />
+                        ) : (
+                          <Plus className="w-4 h-4" />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-right">${item.price}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
@@ -114,25 +118,25 @@ const [isPending, startTransition] = useTransition();
           </div>
 
           <Card>
-            <CardContent className='p-4 gap-4'>
-              <div className='pb-3 text-xl'>
+            <CardContent className="p-4 gap-4">
+              <div className="pb-3 text-xl">
                 Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
-                <span className='font-bold'>
-                  {/* {formatCurrency(cart.itemsPrice)} */}
+                <span className="font-bold">
+                  {formatCurrency(cart.itemsPrice)}
                 </span>
               </div>
               <Button
-                className='w-full'
+                className="w-full"
                 disabled={isPending}
                 onClick={() =>
-                  startTransition(() => router.push('/shipping-address'))
+                  startTransition(() => router.push("/shipping-address"))
                 }
               >
                 {isPending ? (
-                  <Loader className='w-4 h-4 animate-spin' />
+                  <Loader className="w-4 h-4 animate-spin" />
                 ) : (
-                  <ArrowRight className='w-4 h-4' />
-                )}{' '}
+                  <ArrowRight className="w-4 h-4" />
+                )}{" "}
                 Proceed to Checkout
               </Button>
             </CardContent>
