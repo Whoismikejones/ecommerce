9280aafd09f2525741cb3defbf20db4ca178f44f
Whoismikejones <whoismikejones25@gmail.com>
2025-09-30 18:46:26 -0500

    section 7 68

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,32 +1,67 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Prostore';
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Prostore";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-  'A modern ecommerce store built with Next.js';
+  "A modern ecommerce store built with Next.js";
 export const SERVER_URL =
-  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
-//cards displayed need count from page.tsx and index.ts  
+//cards displayed need count from page.tsx and index.ts
 export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
-  export const signInDefaultValues = {
-    email: '',
-    password: '',
-  };
+export const signInDefaultValues = {
+  email: "",
+  password: "",
+};
 
-    export const signUpDefaultValues = {
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  };
+export const signUpDefaultValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
-    export const shippingAddressDefaultValues = {
-    // companyName: '',
-    fullName: '',
-    companyName: '',
-    streetAddress: '',
-    city: '',
-    postalCode: '',
-    country: 'United States',
-  };
+export const shippingAddressDefaultValues = {
+  // companyName: '',
+  fullName: "",
+  companyName: "",
+  streetAddress: "",
+  city: "",
+  postalCode: "",
+  country: "United States",
+};
+
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
+  ? process.env.PAYMENT_METHODS.split(", ")
+  : ["Pay Pal", "Stripe", "Cash On Delivery", "Credit Card"];
+export const DEFAULT_PAYMENT_METHOD =
+  process.env.DEFAULT_PAYMENT_METHOD || "PayPal";
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 12;
+
+export const productDefaultValues = {
+  name: "",
+  slug: "",
+  category: "",
+  images: [],
+  brand: "",
+  description: "",
+  price: "0",
+  stock: 0,
+  rating: "0",
+  numReviews: "0",
+  isFeatured: false,
+  banner: null,
+};
+
+export const USER_ROLES = process.env.USER_ROLES
+  ? process.env.USER_ROLES.split(", ")
+  : ["admin", "user"];
+
+export const reviewFormDefaultValues = {
+  title: "",
+  comment: "",
+  rating: 0,
+};
+
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || "onboarding@resend.dev";
