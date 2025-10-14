f19ceb1082a82a3b1b5c313c4b56560e186650ee
Whoismikejones <whoismikejones25@gmail.com>
2025-10-14 17:38:11 -0500

    section8 update

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -4,7 +4,7 @@ import { notFound, redirect } from 'next/navigation';
 import OrderDetailsTable from './order-details-table';
 import { ShippingAddress } from '@/types';
 import { auth } from '@/auth';
-import Stripe from 'stripe';
+// import Stripe from 'stripe';
 
 export const metadata: Metadata = {
   title: 'Order Details',
@@ -27,32 +27,33 @@ const OrderDetailsPage = async (props: {
     return redirect('/unauthorized');
   }
 
-  let client_secret = null;
+  // let client_secret = null;
 
-  // Check if is not paid and using stripe
-  if (order.paymentMethod === 'Stripe' && !order.isPaid) {
-    // Init stripe instance
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
-    // Create payment intent
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: Math.round(Number(order.totalPrice) * 100),
-      currency: 'USD',
-      metadata: { orderId: order.id },
-    });
-    client_secret = paymentIntent.client_secret;
-  }
+  // // Check if is not paid and using stripe
+  // if (order.paymentMethod === 'Stripe' && !order.isPaid) {
+  //   // Init stripe instance
+  //   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+  //   // Create payment intent
+  //   const paymentIntent = await stripe.paymentIntents.create({
+  //     amount: Math.round(Number(order.totalPrice) * 100),
+  //     currency: 'USD',
+  //     metadata: { orderId: order.id },
+  //   });
+  //   client_secret = paymentIntent.client_secret;
+  // }
 
-  return (
-    <OrderDetailsTable
-      order={{
-        ...order,
-        shippingAddress: order.shippingAddress as ShippingAddress,
-      }}
-      stripeClientSecret={client_secret}
-      paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
-      isAdmin={session?.user?.role === 'admin' || false}
-    />
-  );
+return (
+  <OrderDetailsTable
+    order={{
+      ...order,
+      shippingAddress: order.shippingAddress as ShippingAddress,
+    }}
+    paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
+   
+  />
+);
 };
+
+
 
 export default OrderDetailsPage;
--- types/index.ts
+++ types/index.ts
@@ -32,7 +32,9 @@ export type Order = z.infer<typeof insertOrderSchema> & {
   user: { name: string; email: string };
   paymentResult: PaymentResult;
 };
+
 export type PaymentResult = z.infer<typeof paymentResultSchema>;
+
 export type Review = z.infer<typeof insertReviewSchema> & {
   id: string;
   createdAt: Date;
